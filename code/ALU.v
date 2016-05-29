dd77893e8319acdb95dd61dc7d7f46e5a25e347a
Kevin Chen <me@kevchentw.com>
2016-05-29 17:51:55 +0800

    add mul

--- code/ALU.v
+++ code/ALU.v
@@ -39,6 +39,7 @@ always @(*) begin
     4'b0010: result_o = src1_i + src2_i;           // ADD
     4'b0110: result_o = src1_i - src2_i;           // SUB
     4'b0111: result_o = src1_i < src2_i ? 1 : 0;   // SLT
+	4'b1011: result_o = src1_i * src2_i;           // MUL
     default: result_o = 0;
   endcase
 end
